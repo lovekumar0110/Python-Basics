e8bb8555a7f88744e38b89c44a6fdbb7a25e78f5
lovekumar0110 <lovelygeewan729@gmail.com>
2026-05-12 00:18:53 +0500

    Create name_validator.py

---
+++ name_validator.py
@@ -0,0 +1,7 @@
+character=input("Enter the character/name")
+if len(character )<3:
+    print("Name must be atleast 3 character ")
+elif len (character)>50 :
+    print("The maxmimum number of character is 50 ")
+else:
+    print("Name looking Good ")
